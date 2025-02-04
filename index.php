99bb3597703b84d21c779f7d9ff044a7c4eda728
thinh204 <huythinh592@gmail.com>
2025-02-05 02:49:01 +0700

    FOOTER DESIGN

--- index.php
+++ index.php
@@ -431,7 +431,7 @@
                 <h5 class="mt-3">Wifi</h5>
             </div>
             <div class="col-lg-12 text-center mt-5">
-            <a href="" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities >>></a>
+                <a href="" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities >>></a>
             </div>
         </div>
     </div>
@@ -501,6 +501,9 @@
             </div>
             <div class="swiper-pagination"></div>
         </div>
+        <div class="col-lg-12 text-center mt-5">
+            <a href="" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">Know More >>></a>
+        </div>
     </div>
 
     <!-- Reach us -->
@@ -547,9 +550,43 @@
         </div>
     </div>
 
+    <!-- Footer Design -->
 
-<br><br><br>
-<br><br><br>
+    <div class="container-fluid bg-white mt-5">
+        <div class="row">
+            <div class="col-lg-4 p-4">
+                <h3 class="h-font fw-bold fs-3 mb-2">Hotel Hopper</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Autem, nobis suscipit, 
+                    aliquid itaque magni eos minima ea porro quos mollitia tenetur soluta nesciunt. 
+                    Natus cumque aut, quod in ratione ex!
+                </p>
+            </div>
+            <div class="col-lg-4 p-4">
+                <h5 class="mb-3">Links</h5>
+                <a href="" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a> <br>
+                <a href="" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a> <br>
+                <a href="" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a> <br>
+                <a href="" class="d-inline-block mb-2 text-dark text-decoration-none">Contact us</a> <br>
+                <a href="" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+            </div>
+            <div class="col-lg-4 p-4">
+                <h5 class="mb-3">Follow us</h5>
+                <a href="" class="d-inline-block text-dark text-decoration-none mb-2">
+                    <i class="bi bi-twitter me-1"></i> Twitter
+                </a><br>
+                <a href="" class="d-inline-block text-dark text-decoration-none mb-2">
+                    <i class="bi bi-facebook me-1"></i> Facebook
+                </a><br>
+                <a href="" class="d-inline-block text-dark text-decoration-none">
+                    <i class="bi bi-instagram me-1"></i> Instagram
+                </a><br>
+            </div>
+        </div>
+    </div>
+
+    <h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developed by Hotel Hopper</h6>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
