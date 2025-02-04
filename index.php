1be0a2c928c7316c188f7a4fc4d003a12bc62d82
thinh204 <huythinh592@gmail.com>
2025-02-05 00:01:08 +0700

    TESTIMONIALS

--- index.php
+++ index.php
@@ -404,6 +404,8 @@
         </div>
     </div>
 
+    <!-- Our Facilities -->
+
     <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">OUR FACILITIES</h2>
     
     <div class="container">
@@ -434,6 +436,73 @@
         </div>
     </div>
 
+    <!-- Testimonials -->
+
+    <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">TESTIMONIALS</h2>
+
+    <div class="container mt-5">
+        <div class="swiper swiper-testimonials">
+            <div class="swiper-wrapper mb-5">
+                <div class="swiper-slide bg-white p-4">
+                    <div class="profile d-flex align-items-center mb-3">
+                        <img src="images/features/star.svg" width="30px">
+                        <h6 class="m-0 ms-2">Random user1</h6>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores dignissimos quidem animi blanditiis, 
+                        molestiae debitis autem, voluptatem inventore nobis aliquam,
+                        voluptatum alias placeat a dolore ducimus magni optio accusamus necessitatibus!
+                    </p>
+                    <div class="rating">
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i> 
+                        <i class="bi bi-star-fill text-warning"></i> 
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>  
+                    </div>
+                </div>
+                <div class="swiper-slide bg-white p-4">
+                    <div class="profile d-flex align-items-center mb-3">
+                        <img src="images/features/star.svg" width="30px">
+                        <h6 class="m-0 ms-2">Random user1</h6>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores dignissimos quidem animi blanditiis, 
+                        molestiae debitis autem, voluptatem inventore nobis aliquam,
+                        voluptatum alias placeat a dolore ducimus magni optio accusamus necessitatibus!
+                    </p>
+                    <div class="rating">
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i> 
+                        <i class="bi bi-star-fill text-warning"></i> 
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>  
+                    </div>
+                </div>
+                <div class="swiper-slide bg-white p-4">
+                    <div class="profile d-flex align-items-center mb-3">
+                        <img src="images/features/star.svg" width="30px">
+                        <h6 class="m-0 ms-2">Random user1</h6>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores dignissimos quidem animi blanditiis, 
+                        molestiae debitis autem, voluptatem inventore nobis aliquam,
+                        voluptatum alias placeat a dolore ducimus magni optio accusamus necessitatibus!
+                    </p>
+                    <div class="rating">
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i> 
+                        <i class="bi bi-star-fill text-warning"></i> 
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>  
+                    </div>
+                </div>
+                
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+    </div>
+
 
 <br><br><br>
 <br><br><br>
@@ -442,13 +511,47 @@
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
     <script>
+
         var swiper = new Swiper(".swiper-container", {
-        spaceBetween: 30,
-        effect: "fade",
-        loop: true,
-        autoplay: {
+            spaceBetween: 30,
+            effect: "fade",
+            loop: true,
+            autoplay: {
             delay: 3500,
             disableOnInteraction: false,
+        }
+        });
+
+        var swiper = new Swiper(".swiper-testimonials", {
+            effect: "coverflow",
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: "auto",
+            slidesPerView: "3",
+            loop: true,
+            coverflowEffect: {
+                rotate: 50,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: false,
+            },
+        pagination: {
+            el: ".swiper-pagination",
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            640: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
         }
         });
     </script>
