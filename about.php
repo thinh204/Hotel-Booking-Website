6c8dc8342749090f7c067a78ccd6955635f277d2
thinh204 <huythinh592@gmail.com>
2025-02-05 18:04:57 +0700

    ABOUT

--- about.php
+++ about.php
@@ -7,10 +7,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
     <?php require('inc/links.php'); ?>
     <style>
-        .pop:hover {
+        .box {
             border-top-color: var(--teal) !important;
-            transform: scale(1.03);
-            transform: all 0.3s;
         }
     </style>
 </head>                           
@@ -19,7 +17,7 @@
     <?php require('inc/header.php'); ?>
 
     <div class="my-5 px-4">
-        <h2 class="fw-bold h-font text-center">TIỆN ÍCH</h2>
+        <h2 class="fw-bold h-font text-center">GIỚI THIỆU</h2>
         <div class="h-line bg-dark"></div>
         <p class="text-center mt-3">
             Lorem ipsum dolor sit amet consectetur, 
@@ -29,85 +27,102 @@
     </div>
 
     <div class="container">
+        <div class="row justify-content-between align-items-center">
+            <div class="col-lg-6 col-md-5 mb-4 order-lg-1 order-md-1 order-2">
+                <h3 class="mb-3">Lorem ipsum dolor sit</h3>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore repellendus pariatur, 
+                    neque, omnis laboriosam, illo itaque iure aliquid aspernatur nisi quidem in. 
+                    Recusandae eligendi qui minus quaerat dolorem, error cupiditate.
+                </p>
+            </div>
+            <div class="col-lg-5 col-md-5 mb-4 order-lg-2 order-md-2 order-1">
+                <img src="images/about/about.jpg" class="w-100">
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-5">
         <div class="row">
-            <div class="col-lg-4 col-md-6 mb-5 px=4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                        <img src="images/features/wifi-logo.svg" width="40px">
-                        <h5 class="m-0 ms-3">Wifi</h5>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Consectetur, similique? Nobis eaque iusto minus aut tenetur officia libero, 
-                        recusandae itaque deserunt, enim officiis quam exercitationem molestiae doloremque alias totam esse?
-                    </p>
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+                    <img src="images/about/hotel.svg" width="70px">
+                    <h4 class="mt-3">100+ PHÒNG</h4>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 mb-5 px=4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                        <img src="images/features/wifi-logo.svg" width="40px">
-                        <h5 class="m-0 ms-3">Wifi</h5>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Consectetur, similique? Nobis eaque iusto minus aut tenetur officia libero, 
-                        recusandae itaque deserunt, enim officiis quam exercitationem molestiae doloremque alias totam esse?
-                    </p>
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+                    <img src="images/about/customers.svg" width="70px">
+                    <h4 class="mt-3">200+ KHÁCH HÀNG</h4>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 mb-5 px=4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                        <img src="images/features/wifi-logo.svg" width="40px">
-                        <h5 class="m-0 ms-3">Wifi</h5>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Consectetur, similique? Nobis eaque iusto minus aut tenetur officia libero, 
-                        recusandae itaque deserunt, enim officiis quam exercitationem molestiae doloremque alias totam esse?
-                    </p>
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+                    <img src="images/about/reviews.svg" width="70px">
+                    <h4 class="mt-3">150+ NHẬN XÉT</h4>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 mb-5 px=4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                        <img src="images/features/wifi-logo.svg" width="40px">
-                        <h5 class="m-0 ms-3">Wifi</h5>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Consectetur, similique? Nobis eaque iusto minus aut tenetur officia libero, 
-                        recusandae itaque deserunt, enim officiis quam exercitationem molestiae doloremque alias totam esse?
-                    </p>
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+                    <img src="images/about/staff.svg" width="70px">
+                    <h4 class="mt-3">200+ NHÂN VIÊN</h4>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 mb-5 px=4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                        <img src="images/features/wifi-logo.svg" width="40px">
-                        <h5 class="m-0 ms-3">Wifi</h5>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Consectetur, similique? Nobis eaque iusto minus aut tenetur officia libero, 
-                        recusandae itaque deserunt, enim officiis quam exercitationem molestiae doloremque alias totam esse?
-                    </p>
+        </div>
+    </div>
+
+    <h3 class="my-5 fw-bold h-font text-center">ĐỘI NGŨ QUẢN LÝ</h3>
+
+    <div class="container px-4">
+        <div class="swiper mySwiper">
+            <div class="swiper-wrapper mb-5">
+                <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+                    <img src="images/about/team.jpg" class="w-100">
+                    <h5 class="mt-2">Romdom Name</h5>
+                </div>
+                <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+                    <img src="images/about/team.jpg" class="w-100">
+                    <h5 class="mt-2">Romdom Name</h5>
+                </div>
+                <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+                    <img src="images/about/team.jpg" class="w-100">
+                    <h5 class="mt-2">Romdom Name</h5>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 mb-5 px=4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                        <img src="images/features/wifi-logo.svg" width="40px">
-                        <h5 class="m-0 ms-3">Wifi</h5>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Consectetur, similique? Nobis eaque iusto minus aut tenetur officia libero, 
-                        recusandae itaque deserunt, enim officiis quam exercitationem molestiae doloremque alias totam esse?
-                    </p>
-                </div>
-            </div>
+            <div class="swiper-pagination"></div>
         </div>
     </div>
 
     <!-- Footer Design -->
 
     <?php require('inc/footer.php'); ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+    <!-- Initialize Swiper -->
+    <script>
+        var swiper = new Swiper(".mySwiper", {
+            slidesPerView: 4,
+            spaceBetween: 40,
+            pagination: {
+                el: ".swiper-pagination",
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                640: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 3,
+                },
+                1024: {
+                    slidesPerView: 3,
+                },
+            }
+        });
+    </script>
     
 </body>
 </html>
