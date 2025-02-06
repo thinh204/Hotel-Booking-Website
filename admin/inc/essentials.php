207b5700c6aa556205774fd7434201f1a3b84dd2
thinh204 <huythinh592@gmail.com>
2025-02-07 01:05:12 +0700

    DASHBOARD

--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -1,11 +1,25 @@
 <?php
 
+    function adminLogin() 
+    {
+        session_start();
+        if(!(isset($_SESSION['adminLogin']) && $_SESSION['adminLogin']==true)) {
+            echo "<script>
+                window.location.href='index.php';
+            </script>";
+
+        }
+        session_regenerate_id(true);
+    }
+
     function redirect($url) {
         if (!headers_sent()) {
             header("Location: $url");
             exit;
         } else {
-            echo "<script>window.location.href='$url';</script>";
+            echo "<script>
+                window.location.href='$url';
+            </script>";
             exit;
         }
     }
--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,12 @@
 <?php
     require('inc/essentials.php');
     require('inc/db_config.php');
+
+    session_start();
+    session_regenerate_id(true);
+    if((isset($_SESSION['adminLogin']) && $_SESSION['adminLogin']==true)) {
+       redirect('dashboard.php'); 
+    }
 ?>
 
 
@@ -45,16 +51,13 @@
             $frm_data = filteration($_POST);
 
             $query = "SELECT * FROM admin_cred WHERE admin_name=? AND admin_pass=?";
-
             $values = [$frm_data['admin_name'],$frm_data['admin_pass']];
 
             $res = select($query,$values,"ss");
             if($res->num_rows==1){
                 $row = mysqli_fetch_assoc($res);
-                session_start();
                 $_SESSION['adminLogin'] = true;
                 $_SESSION['adminId'] = $row['sr_no'];
-
                 redirect('dashboard.php');
             }
             else {
